25c10f98bce0d9dc73a0eda8bae6b823ac164b0e
fazlulkarimyousuf <faizmiah52@gmail.com>
2025-06-04 14:45:17 +0600

    register section done and firebase auth start

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATp0cg4Pgth3WLaaAfwiOE5IRGOj1usaM",
+  authDomain: "career-code-f8b92.firebaseapp.com",
+  projectId: "career-code-f8b92",
+  storageBucket: "career-code-f8b92.firebasestorage.app",
+  messagingSenderId: "175519061720",
+  appId: "1:175519061720:web:fc52f5fd8c0511acb369ad"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
